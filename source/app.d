3f120eeec5ff89a3c828e203e501d5fac287ba00
Saurabh Totey <SaurabhTotey@gmail.com>
2017-12-18 17:41:20 -0700

    Added comments and documentation

--- source/app.d
+++ source/app.d
@@ -5,18 +5,25 @@ import std.datetime;
 import core.thread;
 import d2d;
 
+/**
+ * The actual activity the user will see
+ * Determines what the window actually does
+ */
 class MainScreen : Screen {
 
-    enum width = 100;
-    enum ticksPerSecond = 20;
-    Texture hammerAndSickle;
-    Texture eagle;
-    Sound!(SoundType.Music) ussrAnthem;
-    int wheelDisplacement;
-    iRectangle location;
-    iPoint velocity;
-    iRectangle[] blocks;
+    enum width = 100; ///How wide each entity in this demo is
+    enum ticksPerSecond = 20; ///How many times the physics should update in a second
+    Texture hammerAndSickle; ///The image of the hammer and sickle
+    Texture eagle; ///The image of an eagle
+    Sound!(SoundType.Music) ussrAnthem; ///The USSR anthem music
+    int wheelDisplacement; ///How much the mouse wheel has been displaced total in the y direction
+    iRectangle location; ///Where the image of the hammer and sickle is
+    iPoint velocity; ///The velocity of the hammer and sickle as components
+    iRectangle[] blocks; ///The locations of all of the eagle textures
 
+    /**
+     * Deletes all of the eagle locations and starts the hammer and sickle in a new place with a new velocity
+     */
     void randomize() {
         this.location = new iRectangle(uniform(0,
                 this.container.window.size.x - width), uniform(0,
@@ -26,27 +33,43 @@ class MainScreen : Screen {
         this.blocks = null;
     }
 
+    /**
+     * Plays the collision or clapping sound
+     */
     void playCollisionSound() {
         new Sound!(SoundType.Chunk)("res/Clap.wav");
     }
 
+    /**
+     * Where the actual logic of the screen is handled
+     * Isn't required by the Screen abstract class, but is a function for clarity's sake
+     * Is only called in a thread in the constructor; shouldn't be called anywhere else and should only be called once
+     */
     void runDemo() {
         SysTime lastTickTime;
+        //Makes sure to not hold the program up; runs while the window is open
         while (container.isRunning) {
+            //Limits the logic rate to whatever ticksPerSecond is set to
             if (Clock.currTime() >= lastTickTime + dur!"msecs"((1000 / ticksPerSecond))) {
+                //If the block collides with a wall on either x side, it flips the x-velocity of it
                 if (location.x <= 0 || location.x + width >= container.window.size.x) {
                     velocity.x *= -1;
                     playCollisionSound();
                 }
+                //If the block collides with a wall on either y side, it flips the y-velocity of it
                 if (location.y <= 0 || location.y + width >= container.window.size.y) {
                     velocity.y *= -1;
                     playCollisionSound();
                 }
+                //Removes any blocks that are intersecting with the hammer and sickle
                 ulong numBlocks = blocks.length;
                 blocks = blocks.filter!(block => !location.intersects(block)).array;
+                //If any blocks got removed, a collision happened and thus the collision sound gets played
                 if (numBlocks > blocks.length) {
                     playCollisionSound();
                 }
+                //Updates the block's location based on it's velocity
+                //TODO There is a better way to do this based on percentage of time until the next tick
                 location.x += velocity.x;
                 location.y += velocity.y;
                 lastTickTime = Clock.currTime();
@@ -54,6 +77,10 @@ class MainScreen : Screen {
         }
     }
 
+    /** 
+     * Constructs this screen given the container
+     * Handles initializing all of the demo components such as textures, music, and logic
+     */
     this(Display container) {
         super(container);
         this.hammerAndSickle = new Texture(loadImage("res/HammerAndSickle.png"),
@@ -61,22 +88,31 @@ class MainScreen : Screen {
         this.eagle = new Texture(loadImage("res/Eagle.png"), this.container.window.renderer);
         this.randomize();
         this.ussrAnthem = new Sound!(SoundType.Music)("res/USSR-Anthem.mp3");
-        musicVolume = MIX_MAX_VOLUME / 4;
+        musicVolume = MIX_MAX_VOLUME / 4; //Music is a bit loud
+        //Runs logic in a separate thread because it handles timing independently
         new Thread(&this.runDemo).start();
     }
 
+    /**
+     * Allows the screen to react to incoming events
+     * Defines special behaviour for specific events
+     */
     override void handleEvent(SDL_Event event) {
+        //If space is pressed, the hammer and sickle location gets randomized and all of the eagles get deleted
         if (container.keyboard.allKeys.filter!(key => key.id == SDLK_SPACE).front.testAndRelease()) {
             this.randomize();
         }
+        //If escape is pressed, the display gets marked to close
         if (container.keyboard.allKeys.filter!(key => key.id == SDLK_ESCAPE).front.testAndRelease()) {
             this.container.isRunning = false;
         }
+        //If the left mouse button is clicked, an eagle is placed at the location of the mouse
         if (container.mouse.allButtons.filter!(button => button.id == SDL_BUTTON_LEFT)
                 .front.testAndRelease()) {
             this.blocks ~= new iRectangle(container.mouse.windowLocation.x - width / 2,
                     container.mouse.windowLocation.y - width / 2, width, width);
         }
+        //If the right mouse button is clicked, it removes any eagle that is at the mouse location
         if (container.mouse.allButtons.filter!(button => button.id == SDL_BUTTON_RIGHT)
                 .front.testAndRelease()) {
             iRectangle[] toRemove = this.blocks.filter!(
@@ -86,6 +122,7 @@ class MainScreen : Screen {
                 this.blocks = this.blocks.remove(this.blocks.countUntil(toRemove[$ - 1]));
             }
         }
+        //If the mousewheel displacement got changed, it adjusts the music volume based on the change of the mouse wheel
         if (container.mouse.totalWheelDisplacement.y != wheelDisplacement) {
             musicVolume = musicVolume + container.mouse.totalWheelDisplacement.y - wheelDisplacement;
             if (musicVolume < 0)
@@ -96,10 +133,18 @@ class MainScreen : Screen {
         }
     }
 
+    /**
+     * What the screen should do every frame update of the application
+     * This screen in particular doesn't do anything in regards to what it should do every frame
+     */
     override void onFrame() {
-
     }
 
+    /**
+     * Handles actually drawing whatever the screen wants to draw to the screen
+     * This screen draws the eagles and then the hammer and sickle
+     * Hammer and sickle, as it gets drawn after the eagles, would go on top
+     */
     override void draw() {
         this.blocks.each!(block => this.container.window.renderer.copy(this.eagle, block));
         this.container.window.renderer.copy(this.hammerAndSickle, this.location);
@@ -107,9 +152,15 @@ class MainScreen : Screen {
 
 }
 
+/**
+ * Entry point for the program
+ */
 void main() {
+    //Creates a display that is initially 640 by 480, resizable, has a magnificent title, and has an icon of Lenin
     Display mainDisplay = new Display(640, 480, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE,
             "Saurabh Totey's Testarific Testarooni", "res/Lenin.jpg");
+    //Sets the display's screen to the MainScreen that is defined above
     mainDisplay.screen = new MainScreen(mainDisplay);
+    //Starts running the display so that it can handle collecting events and limiting framerate and such
     mainDisplay.run();
 }
